9321ff583eed291ef057574e08cea6cfc2078d02
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2022-08-27 12:56:53 -0500

    Adding header and footer.

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -8,6 +8,21 @@ module.exports = {
     './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
+    colors: {
+      "mainColor": "#2c3639",
+      "white": "#ffffff",
+      "black": "#000000",
+      "signIn": "#9ff1e5",
+      "signUp": "#ecffc8",
+      "fontColor": "#0c5d75",
+      "footerGray": "#f5f5f5",
+      "editIcon": "#047487",
+      "removeIcon": "#920404",
+      "evaluationStrong": "#126f05",
+      "evaluationLight": "#ddffd9",
+      "chapterStrong": "#05636f",
+      "surveyStrong": "#686f05",
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
