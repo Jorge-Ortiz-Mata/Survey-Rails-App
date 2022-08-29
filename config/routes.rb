8ecfc5aeb66d663359312d26bc45c1ec3b26e2e9
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2022-08-29 16:02:24 -0500

    Adding sign out and different routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'pages/index'
-  root 'pages#landing_page'
+
+  authenticated(:user) do
+    root "pages#index", as: :authenticated_root
+  end
+
+  unauthenticated(:user) do
+    root "pages#landing_page"
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
