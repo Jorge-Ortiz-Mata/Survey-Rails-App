9bee556b18795c1b8fda48c2b940d3cae1e66c59
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2022-09-06 13:04:07 -0500

    Style question form.

--- app/javascript/controllers/reset_controller.js
+++ app/javascript/controllers/reset_controller.js
@@ -4,4 +4,11 @@ export default class extends Controller {
   resetForm() {
     document.getElementById('modal').remove();
   }
+
+  questionForm(){
+    const timer = setInterval(() => {
+      document.getElementById('new_question').reset();
+      clearInterval(timer);
+    }, 100)
+  }
 }
