23857d97b7a5f29091f59cb750e4f41b83ed9fc8
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2022-09-07 17:15:20 -0500

    C for options.

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -1,5 +1,27 @@
 class OptionsController < ApplicationController
+  before_action :set_question, only: %i[create]
+
   def create
-    puts "hello world"
+    @option = @question.options.new(option_params)
+
+    if @option.save
+      respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.append('options_all', 
+                                                  partial: 'options/option', 
+                                                  locals: { option: @option }) }
+      end
+    else
+      format.html { render :new, status: :unprocessable_entity }
+    end
+  end
+
+  private
+
+  def set_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def option_params
+    params.require(:option).permit(:name)
   end
 end
