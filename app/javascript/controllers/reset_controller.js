95cdcc528afd96390b2232cfb20c535c1f2e4cb8
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2022-09-08 16:48:45 -0500

    Adding Stimulus JS to reset option's form.

--- app/javascript/controllers/reset_controller.js
+++ app/javascript/controllers/reset_controller.js
@@ -1,13 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
 
+const restoreForm = (id) => {
+  document.getElementById(`${id}`).reset();
+}
+
 export default class extends Controller {
   resetForm() {
     document.getElementById('modal').remove();
   }
 
   questionForm(){
+    const formId = "new_question";
     const timer = setInterval(() => {
-      document.getElementById('new_question').reset();
+      restoreForm(formId);
+      clearInterval(timer);
+    }, 100)
+  }
+
+  optionForm(e){
+    const formId = e.target.id;
+    const timer = setInterval(() => {
+      restoreForm(formId);
       clearInterval(timer);
     }, 100)
   }
